b1852c0296d6b98ac931e704353e1715e88c5b33
juanaugusto <juan.paula@corp.globo.com>
2017-03-03 13:16:02 -0300

    Creating some more files

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log("It works!");
+var express = require('express');
+var app = express();
+var path = require('path');
+app.set('port', 3000);
+
+// app.get('/', function (req, res) {
+//    console.log("GET the homepage");
+//    res.
+//         status(200).
+//         sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+app.use(function (req, res, next) {
+   console.log(req.method, req.url);
+   next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/json', function (req, res) {
+    console.log("GET the homepage");
+    res.
+    status(200).
+    json({"jsonData": true});
+});
+
+app.get('/file', function (req, res) {
+    console.log("GET the homepage");
+    res.
+    status(200).
+    sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function (){
+    var port = server.address().port;
+    console.log('Magic happens on port '+port);
+});
+
+
