f8c5015b231391bc7776200954feea0b0bf54708
Juan Augusto <juan.augusto@ufrj.br>
2017-04-27 07:41:00 -0300

    Creating files for lesson 01 season 14

--- provision.sh
+++ provision.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd ~/
+
+# Installing NodeJS v7
 sudo apt-get update
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
+# Installing MongoDB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
 
